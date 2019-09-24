908421a14f38a0d71a5f850c443f148ec6145ea9
memerye <lx66@duke.edu>
2019-09-24 13:53:24 -0400

    achieve bonus and modifythe ignored files

--- test_tachycardia.py
+++ test_tachycardia.py
@@ -6,6 +6,9 @@ import pytest
     (" :tacHycardic", True),
     ("taCHyca. rdic", True),
     ("Tach?ycardic ...", True),
+    ("tchycardic", True),   # Miss one word
+    ("t0chycard1c", True),  # Mistake two words
+    ("t0chycrd1c", False),  # Mistake or miss three words
     ("ooooops tachycardic", False)
 ])
 def test_is_tachycardic(s, expected):
