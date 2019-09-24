e49ed58ddda0f9f623dfe68ded72a7948e1eff5a
memerye <lx66@duke.edu>
2019-09-24 17:40:41 -0400

    fix the logical errors in algorithm and correct the assert in test file

--- test_tachycardia.py
+++ test_tachycardia.py
@@ -3,15 +3,15 @@ import pytest
 
 @pytest.mark.parametrize("s, expected", [
     ("tachycardic", True),
-    (" :tacHycardic", True),
-    ("taCHyca. rdic", True),
+    (" taCHyca. rdic", True),
     ("Tach?ycardic ...", True),
-    ("tchycardic", True),   # Miss one word
-    ("t0chycard1c", True),  # Mistake two words
-    ("t0chycrd1c", False),  # Mistake or miss three words
-    ("ooooops tachycardic", False)
+    ("tchycardic", True),    # Miss one word
+    ("tachyQcardic", True),  # Insert one word
+    ("t0chycard1c", True),   # Mistake two words
+    ("t0chycrd1c", False),   # Mistake or miss three words
+    ("ooooops Tachycardic,", True)  # The string contains "tachycardic"
 ])
 def test_is_tachycardic(s, expected):
     from tachycardia import is_tachycardic
     result = is_tachycardic(s)
-    return result == expected
+    assert result == expected
